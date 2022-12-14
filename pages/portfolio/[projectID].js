358f08346a5976a1eb83a85c9e3e42cd66056aad
GulsahDgr <34721537+GulsahDgr@users.noreply.github.com>
2022-12-14 12:14:17 +0300

    initial commit

---
+++ pages/portfolio/[projectID].js
@@ -0,0 +1,17 @@
+import {useRouter} from 'next/router'
+
+function PorfolioProjectPage(){
+    const router = useRouter();
+
+    console.log(router.pathname);
+    console.log(router.query);
+
+
+    return(
+        <div>
+            <h1>Portfolio Project Page</h1>
+        </div>
+    )
+}
+
+export default PorfolioProjectPage;
